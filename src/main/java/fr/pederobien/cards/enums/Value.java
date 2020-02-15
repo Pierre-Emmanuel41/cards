d27bf22714d3fba68cbe3a18b7bb0af969e818a2
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-02-16 00:58:33 +0100

    Renaming enum Height as Eight

--- src/main/java/fr/pederobien/cards/enums/Value.java
+++ src/main/java/fr/pederobien/cards/enums/Value.java
@@ -2,7 +2,7 @@ package fr.pederobien.cards.enums;
 
 public enum Value {
 
-	TWO("2", 2), THREE("3", 3), FOUR("4", 4), FIVE("5", 5), SIX("6", 6), SEVEN("7", 7), HEIGHT("8", 8), NEIN("9", 9), TEN("10", 10), JACK("jack", 11), QUEEN("queen", 12),
+	TWO("2", 2), THREE("3", 3), FOUR("4", 4), FIVE("5", 5), SIX("6", 6), SEVEN("7", 7), EIGHT("8", 8), NEIN("9", 9), TEN("10", 10), JACK("jack", 11), QUEEN("queen", 12),
 	KING("king", 13), ACE("ace", 14);
 
 	private String name;
