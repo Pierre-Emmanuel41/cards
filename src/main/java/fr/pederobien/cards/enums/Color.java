4a9ba16cc47131f2e250384a51854c5118a9a763
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-02-16 00:06:28 +0100

    Adding enumerations Color and Value

---
+++ src/main/java/fr/pederobien/cards/enums/Color.java
@@ -0,0 +1,35 @@
+package fr.pederobien.cards.enums;
+
+public enum Color {
+
+	CLUBS("clubs"), DIAMONDS("diamonds"), HEARTS("hearts"), SPADES("spades");
+
+	private String name;
+	private boolean trump;
+
+	private Color(String name) {
+		this.name = name;
+		trump = false;
+	}
+
+	/**
+	 * @return True if this {@link Color} is a trump color, false otherwise.
+	 */
+	public boolean isTrump() {
+		return trump;
+	}
+
+	/**
+	 * Set the trump property.
+	 * 
+	 * @param trump True if this {@link Color} is a trump color, false otherwise.
+	 */
+	public void setTrump(boolean trump) {
+		this.trump = trump;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+}
---
+++ src/main/java/fr/pederobien/cards/enums/Value.java
@@ -0,0 +1,29 @@
+package fr.pederobien.cards.enums;
+
+public enum Value {
+
+	TWO("2", 2), THREE("3", 3), FOUR("4", 4), FIVE("5", 5), SIX("6", 6), SEVEN("7", 7), HEIGHT("8", 8), NEIN("9", 9), TEN("10", 10), JACK("jack", 11), QUEEN("queen", 12),
+	KING("king", 13), ACE("ace", 14);
+
+	private String name;
+	private int value;
+
+	private Value(String name, int value) {
+		this.name = name;
+		this.value = value;
+	}
+
+	/**
+	 * The value represents cards from the 2 (2) to the ace(14).
+	 * 
+	 * @return The value of this card.
+	 */
+	public int getValue() {
+		return value;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+}
